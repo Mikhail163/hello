e801c6ca9f1b728f35179fae9ad8618f4d459257
MistralB <mephist0_87@mail.ru>
2017-01-20 22:07:17 +0400

    first tromart commit

---
+++ basic/views/site/index.php
@@ -0,0 +1,62 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Путешествия по Самарской луке TROMART';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+    	<img src="/tromart.png" title="Логотип Tromart" alt="Tromart logo" />
+        <h1>Организовываем активный отдых для вашей компании!</h1>
+
+		<p class="lead">Как мы это делаем (в Самарской области):</br></br>
+			Консультируем по снаряжению - подбираем и предоставляем напрокат</br>
+			Согласовываем маршрут</br>
+			Предоставляем лучшего проводника и инструктора</br></br>
+		</p>
+
+
+        <p><a class="btn btn-lg btn-success" href="/site/contact">Хотим классно отдохнуть</a></p>
+    </div>
+<?php
+/*
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Лыжи</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Велосипед</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Байдарка</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+    */
+?>
+</div>
